d3dff9be027680ef088fdcf418bdb171f182f652
NBorow <nicholasb0817@gmail.com>
2025-01-18 13:29:55 -0500

    default song to disc cat, change animation message text

--- src/main/java/org/nc/nccasino/helpers/AnimationSongs.java
+++ src/main/java/org/nc/nccasino/helpers/AnimationSongs.java
@@ -12,9 +12,9 @@ public class AnimationSongs {
 
     static {
         // Register animations per game
-        animationSongMap.put("Roulette", getRouletteIntro());
-        animationSongMap.put("Blackjack", getBlackjackIntro());
-        animationSongMap.put("Mines", getMinesIntro());
+        animationSongMap.put("NCCasino - Roulette", getRouletteIntro());
+        animationSongMap.put("NCCasino - Blackjack", getBlackjackIntro());
+        animationSongMap.put("NCCasino - Mines", getMinesIntro());
     }
 
     public static Song getAnimationSong(String gameType) {
@@ -23,28 +23,26 @@ public class AnimationSongs {
 
     private static Song getRouletteIntro() {
         Song song = new Song("RouletteIntro", 20);
-        song.addNote(new Note(Sound.BLOCK_NOTE_BLOCK_BELL, 0, 2, 1.0f, 1.2f));
-        song.addNote(new Note(Sound.ENTITY_PLAYER_LEVELUP, 10, 12, 0.8f, 1.5f));
+        song.addNote(new Note(Sound.MUSIC_DISC_CAT, 0, 0, 2f, 5f));
         return song;
     }
 
     private static Song getBlackjackIntro() {
         Song song = new Song("BlackjackIntro", 20);
-        song.addNote(new Note(Sound.ITEM_BOOK_PAGE_TURN, 0, 2, 1.0f, 1.0f));
-        song.addNote(new Note(Sound.BLOCK_ANVIL_USE, 10, 12, 0.8f, 1.2f));
+        song.addNote(new Note(Sound.MUSIC_DISC_CAT, 0, 0, 2f, 5f));
         return song;
     }
 
     private static Song getMinesIntro() {
         Song song = new Song("MinesIntro", 20);
-        song.addNote(new Note(Sound.ENTITY_VILLAGER_YES, 0, 2, 1.0f, 1.1f));
-        song.addNote(new Note(Sound.BLOCK_WOODEN_TRAPDOOR_OPEN, 10, 12, 0.8f, 1.3f));
+        song.addNote(new Note(Sound.MUSIC_DISC_CAT, 0, 0, 2f, 5f));
         return song;
     }
 
     private static Song getDefaultIntro() {
         Song song = new Song("DefaultIntro", 20);
-        song.addNote(new Note(Sound.BLOCK_NOTE_BLOCK_PLING, 0, 2, 1.0f, 1.0f));
+        song.addNote(new Note(Sound.MUSIC_DISC_CAT, 0, 0, 2f, 5f));
         return song;
     }
 }
+
