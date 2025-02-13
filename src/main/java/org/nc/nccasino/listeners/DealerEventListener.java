d8726cc2acabb13459a4e97db166f1f310d4cc1d
NBorow <nicholasb0817@gmail.com>
2025-02-13 05:03:26 -0500

    potions

--- src/main/java/org/nc/nccasino/listeners/DealerEventListener.java
+++ src/main/java/org/nc/nccasino/listeners/DealerEventListener.java
@@ -2,10 +2,16 @@ package org.nc.nccasino.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityTransformEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
+
+import java.util.List;
+
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Villager;
 import org.nc.nccasino.entities.Dealer;
@@ -36,6 +42,23 @@ public class DealerEventListener implements Listener {
         if (entity instanceof Mob && Dealer.isDealer((Mob) entity)) {
                 event.setCancelled(true);
         }
+    }
+
+    //regular splash potion
+    @EventHandler
+    public void onPotionSplash(PotionSplashEvent event) {
+        for (LivingEntity entity : event.getAffectedEntities()) {
+            if (entity instanceof Mob && Dealer.isDealer((Mob) entity)) {
+                event.setIntensity(entity, 0); // Nullify the effect
+            }
+        }
+    }
+
+    //lingering potions
+    @EventHandler
+    public void onAreaEffectCloudApply(AreaEffectCloudApplyEvent event) {
+        List<LivingEntity> affectedEntities = event.getAffectedEntities();
+        affectedEntities.removeIf(entity -> entity instanceof Mob && Dealer.isDealer((Mob) entity));
     }
 
 }
