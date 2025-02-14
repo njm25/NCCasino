f26bd0cdb89665418eea14f42a45df4042403899
NBorow <nicholasb0817@gmail.com>
2025-02-13 21:02:40 -0500

    pickup items and all transformations

--- src/main/java/org/nc/nccasino/listeners/DealerEventListener.java
+++ src/main/java/org/nc/nccasino/listeners/DealerEventListener.java
@@ -5,15 +5,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTransformEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 
 import java.util.List;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Fox;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
-import org.bukkit.entity.Villager;
 import org.nc.nccasino.entities.Dealer;
 
 public class DealerEventListener implements Listener {
@@ -28,13 +30,13 @@ public class DealerEventListener implements Listener {
     }
 
     @EventHandler
-    public void onVillagerTransform(EntityTransformEvent event) {
-        if (event.getEntity() instanceof Villager villager) {
-            if (Dealer.isDealer(villager)) {
-                event.setCancelled(true);
-            }
+    public void onEntityTransform(EntityTransformEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Mob && Dealer.isDealer((Mob) entity)) {
+            event.setCancelled(true);
         }
     }
+    
 
     @EventHandler
     public void onEntityCombust(EntityCombustEvent event) {
@@ -59,6 +61,23 @@ public class DealerEventListener implements Listener {
     public void onAreaEffectCloudApply(AreaEffectCloudApplyEvent event) {
         List<LivingEntity> affectedEntities = event.getAffectedEntities();
         affectedEntities.removeIf(entity -> entity instanceof Mob && Dealer.isDealer((Mob) entity));
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        if (event.getEntity() instanceof Mob && Dealer.isDealer((Mob) event.getEntity())) {
+            Mob dealer = (Mob) event.getEntity();
+            dealer.setCanPickupItems(false); // Prevent item pickup
+        }
+    }
+
+    @EventHandler
+    public void onEntityPickupItem(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Fox fox) {
+            if (Dealer.isDealer(fox)) {
+                event.setCancelled(true); // Prevent dealer foxes from picking up items
+            }
+        }
     }
 
 }
