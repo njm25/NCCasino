6ee80e39f576bafc90fe9d7266508c40816734ea
NBorow <nicholasb0817@gmail.com>
2025-02-15 03:04:14 -0500

    mob variant toggle

--- src/main/java/org/nc/nccasino/components/MobSelectionInventory.java
+++ src/main/java/org/nc/nccasino/components/MobSelectionInventory.java
@@ -2,12 +2,30 @@ package org.nc.nccasino.components;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Axolotl;
+import org.bukkit.entity.Cat;
+import org.bukkit.entity.Cat.Type;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fox;
+import org.bukkit.entity.Frog;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Llama;
 import org.bukkit.entity.Mob;
+import org.bukkit.entity.MushroomCow;
+import org.bukkit.entity.Panda;
+import org.bukkit.entity.Parrot;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Rabbit;
+import org.bukkit.entity.Sheep;
+import org.bukkit.entity.TropicalFish;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.ZombieVillager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -17,6 +35,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.nc.nccasino.Nccasino;
 import org.nc.nccasino.entities.Dealer;
 import org.nc.nccasino.entities.DealerInventory;
+import org.nc.nccasino.helpers.SoundHelper;
 
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -26,6 +45,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class MobSelectionInventory extends DealerInventory {
     private final UUID ownerId;
@@ -36,20 +56,45 @@ public class MobSelectionInventory extends DealerInventory {
     private int currentPage = 1;
     private static final int PAGE_SIZE = 45;
     private static final Map<Material, EntityType> spawnEggToEntity = new HashMap<>();
+    private static final Map<EntityType, Material> entityToSpawnEgg = new HashMap<>();
     private static final List<Material> spawnEggList = new ArrayList<>();
     private UUID dealerId;
     private final Player player;
     private enum SlotOption {
         EXIT,
         RETURN,
-        PAGE_TOGGLE
+        PAGE_TOGGLE,
+        VARIANT
     }
 
     private final Map<SlotOption, Integer> slotMapping = Map.of(
         SlotOption.EXIT, 53,
         SlotOption.RETURN, 45,
-        SlotOption.PAGE_TOGGLE, 49
+        SlotOption.PAGE_TOGGLE, 49,
+        SlotOption.VARIANT, 47
     );
+
+    private static final List<Villager.Type> VILLAGER_BIOMES = Arrays.asList(
+        Villager.Type.DESERT,
+        Villager.Type.JUNGLE,
+        Villager.Type.PLAINS,
+        Villager.Type.SAVANNA,
+        Villager.Type.SNOW,
+        Villager.Type.SWAMP,
+        Villager.Type.TAIGA
+    );
+    
+    private static final Map<Villager.Type, Material> BIOME_MATERIALS = new HashMap<>() {{
+        
+        put(Villager.Type.DESERT, Material.SAND);
+        put(Villager.Type.JUNGLE, Material.JUNGLE_LOG);
+        put(Villager.Type.PLAINS, Material.GRASS_BLOCK);
+        put(Villager.Type.SAVANNA, Material.ACACIA_LOG);
+        put(Villager.Type.SNOW, Material.SNOW_BLOCK);
+        put(Villager.Type.SWAMP, Material.MANGROVE_LOG);
+        put(Villager.Type.TAIGA, Material.SPRUCE_LOG);
+    }};
+
 
     static {
         for (Material material : Material.values()) {
@@ -57,22 +102,32 @@ public class MobSelectionInventory extends DealerInventory {
                 try {
                     String entityName = material.name().replace("_SPAWN_EGG", "");
                     EntityType entityType = EntityType.valueOf(entityName);
-                    if (entityType != EntityType.ENDER_DRAGON&& entityType != EntityType.WITHER) {
+    
+                    // Exclude Wither / Ender Dragon
+                    if (entityType != EntityType.ENDER_DRAGON && entityType != EntityType.WITHER) {
                         spawnEggToEntity.put(material, entityType);
                         spawnEggList.add(material);
+    
+                        // Build reverse map
+                        entityToSpawnEgg.put(entityType, material);
                     }
-                } catch (IllegalArgumentException ignored) {}
+                } catch (IllegalArgumentException ignored) {
+                    // If material name doesn't match an EntityType, skip
+                }
             }
         }
     
-        // Sort based on natural word order instead of strict ASCII order
+        // Sort the spawnEggList, etc. (unchanged)
         spawnEggList.sort((m1, m2) -> {
             String name1 = formatEntityName(spawnEggToEntity.get(m1).name());
             String name2 = formatEntityName(spawnEggToEntity.get(m2).name());
             return name1.compareToIgnoreCase(name2);
         });
     }
-    
+
+    public static Material getSpawnEggFor(EntityType type) {
+        return entityToSpawnEgg.getOrDefault(type, Material.EGG);
+    }
 
     public MobSelectionInventory(Player player, Nccasino plugin, UUID dealerId, Consumer<Player> returnToAdmin, String returnName) {
         super(player.getUniqueId(), 54, "Select Model for " +  Dealer.getInternalName((Mob) player.getWorld()
@@ -131,6 +186,8 @@ public class MobSelectionInventory extends DealerInventory {
         } else if (currentPage < (int) Math.ceil(spawnEggList.size() / (double) PAGE_SIZE)) {
             addItemAndLore(Material.ARROW, 1, "Next Page", slotMapping.get(SlotOption.PAGE_TOGGLE));
         }
+        updateVariantButton();
+
     }
 
     @Override
@@ -161,6 +218,10 @@ public class MobSelectionInventory extends DealerInventory {
                     returnToAdmin.accept(player);
                     playDefaultSound(player);
                     return;
+                case VARIANT:
+                    handleVariantClick(player);
+                    playDefaultSound(player);
+                    break;
             }
         }
 
@@ -260,10 +321,305 @@ public class MobSelectionInventory extends DealerInventory {
     
     }
 
-private static String formatEntityName(String entityName) {
-    return Arrays.stream(entityName.toLowerCase().replace("_", " ").split(" "))
-                 .map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
-                 .collect(Collectors.joining(" "));
-}
+    private String getVariantName(Mob mob) {
+        if (mob instanceof Cat cat) {
+            return formatEntityName(cat.getCatType().toString()); // Or cat.getVariant() if applicable
+        } else if (mob instanceof Fox fox) {
+            return formatEntityName(fox.getFoxType().toString());
+        } else if (mob instanceof Frog frog) {
+            return formatEntityName(frog.getVariant().toString());
+        } else if (mob instanceof Parrot parrot) {
+            return formatEntityName(parrot.getVariant().toString());
+        } else if (mob instanceof Rabbit rabbit) {
+            return formatEntityName(rabbit.getRabbitType().toString());
+        } else if (mob instanceof Axolotl axolotl) {
+            return formatEntityName(axolotl.getVariant().toString());
+        } else if (mob instanceof MushroomCow mooshroom) {
+            return formatEntityName(mooshroom.getVariant().toString());
+        } else if (mob instanceof Panda panda) {
+            return formatEntityName(panda.getMainGene().toString());
+        } else if (mob instanceof Villager villager) {
+            return formatEntityName(villager.getVillagerType().toString());
+        } else if (mob instanceof ZombieVillager zombieVillager) {
+            return formatEntityName(zombieVillager.getVillagerType().toString());
+        } else if (mob instanceof Sheep sheep) {
+            return formatEntityName(sheep.getColor().toString());
+        }
+
+        return "Unknown";
+    }
+
+    private boolean hasSingleVariant(Mob mob) {
+        return (mob instanceof Cat)
+            || (mob instanceof Fox)
+            || (mob instanceof Frog)
+            || (mob instanceof Parrot)
+            || (mob instanceof Rabbit)
+            || (mob instanceof Axolotl)
+            || (mob instanceof MushroomCow)
+            || (mob instanceof Panda)
+            || (mob instanceof Villager)
+            || (mob instanceof ZombieVillager)
+            || (mob instanceof Sheep);
+    }
+
+        private boolean isComplicatedVariant(Mob mob) {
+        return (mob instanceof Llama)
+            || (mob instanceof Horse)
+            || (mob instanceof TropicalFish);
+    }
+
+    private void handleVariantClick(Player player) {
+        if (isComplicatedVariant(dealer)) {
+               // Stub: open sub-menu (not implemented here)
+               openVariantMenu(player, dealer);
+           }
+           else if (hasSingleVariant(dealer)) {
+               // Cycle single-variant
+               cycleSingleVariant(player, dealer);
+           }
+           else {
+               // No variants do nothing
+               switch (plugin.getPreferences(player.getUniqueId()).getMessageSetting()) {
+                   case STANDARD -> player.sendMessage("§cInvalid Action.");
+                   case VERBOSE  -> player.sendMessage("§cNo variants to cycle for " + dealer.getType());
+                   default -> {}
+               }
+           }
+       }
+   
+      
+       private void openVariantMenu(Player player, Mob dealer) {
+           // You'll build out a Llama/Horse/TropicalFish inventory here
+          // player.closeInventory();
+           player.sendMessage(ChatColor.YELLOW + "[DEBUG] Opening specialized variant menu soon...");
+           // TODO: implement specialized sub-inventory for Horse, Llama, or Tropical Fish
+       }
+
+       private void cycleVillagerType(Player player, Villager villager) {
+            Villager.Type currentBiome = villager.getVillagerType();
+            int index = VILLAGER_BIOMES.indexOf(currentBiome);
+            Villager.Type newBiome = VILLAGER_BIOMES.get((index + 1) % VILLAGER_BIOMES.size());
+            villager.setVillagerType(newBiome);
+
+            // Update item in the admin menu
+            addItemAndLore(
+                BIOME_MATERIALS.getOrDefault(newBiome, Material.GRASS_BLOCK),
+                1,
+                "Toggle " + formatEntityName(dealer.getType().toString()) + " Variant",
+                slotMapping.get(SlotOption.VARIANT),
+                "Current: §a" + formatEntityName(newBiome.toString())
+            );
+
+            switch (plugin.getPreferences(player.getUniqueId()).getMessageSetting()) {
+                case VERBOSE -> player.sendMessage("§aVariant changed to: " + ChatColor.YELLOW+ formatEntityName(newBiome.toString()));
+                default -> {}
+            }
+        }
+
+    private static final Cat.Type[] CAT_TYPES = {
+        Cat.Type.TABBY,
+        Cat.Type.BLACK,
+        Cat.Type.RED,
+        Cat.Type.SIAMESE,
+        Cat.Type.BRITISH_SHORTHAIR,
+        Cat.Type.CALICO,
+        Cat.Type.PERSIAN,
+        Cat.Type.RAGDOLL,
+        Cat.Type.WHITE,
+        Cat.Type.JELLIE,
+        Cat.Type.ALL_BLACK
+    };
+
+    private static final Frog.Variant[] FROG_VARIANTS = {
+        Frog.Variant.TEMPERATE,
+        Frog.Variant.WARM,
+        Frog.Variant.COLD
+    };       
+
+    private void cycleSingleVariant(Player player, Mob mob) {
+        if (mob instanceof Cat cat) {
+            Type current = cat.getCatType();// or cat.getVariant() if your version uses Cat.Variant
+            int currentIndex = indexOf(CAT_TYPES, current);
+            // Move to the next index
+            int nextIndex = (currentIndex + 1) % CAT_TYPES.length;
+            cat.setCatType(CAT_TYPES[nextIndex]);
+            sendVariantUpdateMessage(player, CAT_TYPES[nextIndex]);
+        }
+        else if (mob instanceof Fox fox) {
+            List<Fox.Type> variants = List.of(Fox.Type.values());
+            Fox.Type curr = fox.getFoxType();
+            int idx = variants.indexOf(curr);
+            Fox.Type next = variants.get((idx + 1) % variants.size());
+            fox.setFoxType(next);
+            sendVariantUpdateMessage(player, next);
+        }
+        else if (mob instanceof Frog frog) {
+            // If you're forced to use Frog.Type, do that. 
+            // Otherwise (if on 1.19+), you'd do frog.getVariant() and store FROG_VARIANTS
+            Frog.Variant current = frog.getVariant();
+            int currentIndex = indexOf(FROG_VARIANTS, current);
+            int nextIndex = (currentIndex + 1) % FROG_VARIANTS.length;
+            frog.setVariant(FROG_VARIANTS[nextIndex]);
+            sendVariantUpdateMessage(player, FROG_VARIANTS[nextIndex]);
+        }
+        else if (mob instanceof Parrot parrot) {
+            List<Parrot.Variant> variants = List.of(Parrot.Variant.values());
+            Parrot.Variant curr = parrot.getVariant();
+            int idx = variants.indexOf(curr);
+            Parrot.Variant next = variants.get((idx + 1) % variants.size());
+            parrot.setVariant(next);
+            sendVariantUpdateMessage(player, next);
+        }
+        else if (mob instanceof Rabbit rabbit) {
+            List<Rabbit.Type> variants = List.of(Rabbit.Type.values());
+            Rabbit.Type curr = rabbit.getRabbitType();
+            int idx = variants.indexOf(curr);
+            Rabbit.Type next = variants.get((idx + 1) % variants.size());
+            rabbit.setRabbitType(next);
+            sendVariantUpdateMessage(player, next);
+        }
+        else if (mob instanceof Axolotl axolotl) {
+            List<Axolotl.Variant> variants = List.of(Axolotl.Variant.values());
+            Axolotl.Variant curr = axolotl.getVariant();
+            int idx = variants.indexOf(curr);
+            Axolotl.Variant next = variants.get((idx + 1) % variants.size());
+            axolotl.setVariant(next);
+            sendVariantUpdateMessage(player, next);
+        }
+        else if (mob instanceof MushroomCow mooshroom) {
+            // Mooshroom has a Mooshroom.Variant
+            List<MushroomCow.Variant> variants = List.of(MushroomCow.Variant.values());
+            MushroomCow.Variant curr = mooshroom.getVariant();
+            int idx = variants.indexOf(curr);
+            MushroomCow.Variant next = variants.get((idx + 1) % variants.size());
+            mooshroom.setVariant(next);
+            sendVariantUpdateMessage(player, next);
+        }
+        else if (mob instanceof Panda panda) {
+            if (panda.getMainGene() == Panda.Gene.BROWN) {
+                // Switch back to normal
+                panda.setMainGene(Panda.Gene.NORMAL);
+                panda.setHiddenGene(Panda.Gene.NORMAL);
+            } else {
+                // Force both main and hidden gene to BROWN
+                panda.setMainGene(Panda.Gene.BROWN);
+                panda.setHiddenGene(Panda.Gene.BROWN);
+            }
+            sendVariantUpdateMessage(player, panda.getMainGene());
+        }
+        else if (mob instanceof Villager villager) {
+            cycleVillagerType(player, villager);
+        }
+        else if (mob instanceof ZombieVillager zombieVillager) {
+            cycleZombieVillagerType(player, zombieVillager);
+        }
+         else if (mob instanceof Sheep sheep) {
+        List<DyeColor> colors = List.of(DyeColor.values());
+        int index = colors.indexOf(sheep.getColor());
+        sheep.setColor(colors.get((index + 1) % colors.size()));
+        sendVariantUpdateMessage(player, sheep.getColor());
+    }
+        // Add more if you have more single-variant mob classes
+
+        else {
+            switch (plugin.getPreferences(player.getUniqueId()).getMessageSetting()) {
+                case VERBOSE -> player.sendMessage("§cMob type " +formatEntityName(mob.getType().toString())  + " does not have any variants.");
+                default -> {}
+            }
+        }
+        updateVariantButton();
+    }
+
+    private void updateVariantButton() {
+        int slot = slotMapping.get(SlotOption.VARIANT);
+    
+        if (dealer instanceof Villager villager) {
+            Villager.Type vt = villager.getVillagerType();
+            Material mat = BIOME_MATERIALS.getOrDefault(vt, Material.GRASS_BLOCK);
+            addItemAndLore(
+                mat, 
+                1,
+                "Toggle " + formatEntityName(dealer.getType().toString()) + " Variant",
+                slot,
+                "Current: §a" + formatEntityName(vt.toString())
+            );
+            return;
+        }
+    
+        if (isComplicatedVariant(dealer)) {
+            addItemAndLore(
+                Material.WRITABLE_BOOK,
+                1,
+                "Open Variant Menu",
+                slot,
+                "Current: §a" + dealer.getType().toString()
+            );
+            return;
+        }
+    
+        if (hasSingleVariant(dealer)) {
+            addItemAndLore(
+                Material.REPEATER,
+                1,
+                "Toggle " + formatEntityName(dealer.getType().toString()) + " Variant",
+                slot,
+                "Current: §a" + getVariantName(dealer)
+            );
+            return;
+        }
+    }
+    
+
+    private void cycleZombieVillagerType(Player player, ZombieVillager zombieVillager) {
+        Villager.Type currentBiome = zombieVillager.getVillagerType();
+        int index = VILLAGER_BIOMES.indexOf(currentBiome);
+        Villager.Type newBiome = VILLAGER_BIOMES.get((index + 1) % VILLAGER_BIOMES.size());
+        zombieVillager.setVillagerType(newBiome);
+    
+        sendVariantUpdateMessage(player, "§aVariant changed to: " + ChatColor.YELLOW+ formatEntityName(newBiome.toString()));
+    }
+    
+
+    private static <E> int indexOf(E[] arr, E value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (Objects.equals(arr[i], value)) {
+                return i;
+            }
+        }
+        return 0; // fallback
+    }
+
+
+    /**
+     * Helper to send the new variant name back to the user (verbose mode).
+     */
+    private void sendVariantUpdateMessage(Player player, Object variantObj) {
+        if (SoundHelper.getSoundSafely("entity.villager.work_cartographer", player) != null) {
+            player.playSound(player.getLocation(), 
+                             Sound.ENTITY_VILLAGER_WORK_CARTOGRAPHER, 
+                             SoundCategory.MASTER, 1.0f, 1.0f);
+        }
+    
+        String variantName;
+        if (variantObj instanceof Enum<?> e) {
+            // True Enum, so we can do .name()
+            variantName = e.name();
+        } else {
+            // Might be CraftCat$CraftType, so fallback to .toString()
+            variantName = variantObj.toString();
+        }
+    
+        switch (plugin.getPreferences(player.getUniqueId()).getMessageSetting()) {
+            case VERBOSE  -> player.sendMessage("§aVariant changed to: " + ChatColor.YELLOW + formatEntityName(variantName));
+            default -> {}
+        }
+    }
+
+    private static String formatEntityName(String entityName) {
+        return Arrays.stream(entityName.toLowerCase().replace("_", " ").split(" "))
+                     .map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
+                     .collect(Collectors.joining(" "));
+    }
 
 }
