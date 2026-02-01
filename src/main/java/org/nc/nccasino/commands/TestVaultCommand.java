775a8412158afae2bd74cc47c407d4e88ff35702
nicomilette <nicomilette7@gmail.com>
2026-02-01 15:41:51 -0500

    chore: non working attempt

---
+++ src/main/java/org/nc/nccasino/commands/TestVaultCommand.java
@@ -0,0 +1,13 @@
+package org.nc.nccasino.commands;
+import org.bukkit.command.CommandSender;
+// import net.milkbowl.vault.chat.Chat;
+
+public class TestVaultCommand implements CasinoCommand {
+
+    @Override
+    public boolean execute(CommandSender sender, String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
